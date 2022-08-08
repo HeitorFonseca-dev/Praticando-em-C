2fc407bf28e322a1572a1541dcf00327c460ffcd
xHec <86064837+xHec@users.noreply.github.com>
2022-08-08 17:31:49 -0300

    Update Cordinates of a point.c

--- Cordinates of a point.c
+++ Cordinates of a point.c
@@ -2,24 +2,24 @@
 
 int main (){
 
-float x = 0,y = 0;
+float eixo_x = 0,eixo_y = 0;
 
-scanf ("%f %f",&x ,&y);
+scanf ("%f %f",&eixo_x ,&eixo_y);
 
 
-if (x == 0.0 && y == 0.0){
+if (eixo_x == 0.0 && eixo_y == 0.0){
     printf ("Origem");
 }
-else if ( x == 0.0 && y != 0.0)
+else if ( eixo_x == 0.0 && eixo_y != 0.0)
     printf ("Eixo Y");
-else if ( x != 0.0 && y == 0.0)
+else if ( eixo_x != 0.0 && eixo_y == 0.0)
     printf ("Eixo X");
 
-else if (x > 0 && y > 0)
+else if (eixo_x > 0 && eixo_y > 0)
     printf ("Q1");
-else if (x > 0 && y < 0 )
+else if (eixo_x > 0 && eixo_y < 0 )
     printf ("Q4");
-else if ( x < 0 && y > 0)
+else if ( eixo_x < 0 && eixo_y > 0)
     printf ("Q2");
 else {
     printf ("Q3");
